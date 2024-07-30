878251b8c3b381c4557bdfa3621d45e1498d0eba
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-30 11:49:32 +0000

    Add translation support..

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,46 +1,30 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
-
+"""Flask app with gettext"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext as _
 
+class Config:
+    """Babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-class Config(object):
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
-
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """Select best locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 @app.route('/')
 def index():
-    """_summary_
-    """
+    """Render index page"""
     return render_template('3-index.html')
 
-
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(debug=True)
+
