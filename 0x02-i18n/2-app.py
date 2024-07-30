898891a14c87fb2a68cdd8e7c63c6125b3abafe8
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-30 11:14:40 +0000

    Implement locale selection function.

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,26 +4,31 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
     """Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """Select best locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Render index page"""
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
