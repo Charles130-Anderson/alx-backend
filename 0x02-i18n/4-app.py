1767e2b0559c1a710840923e34af5d29e3b4f0f0
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-30 12:50:06 +0000

    Add locale parameter support

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,51 +1,36 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
-
+"""Flask app with gettext"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
-class Config(object):
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """Babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """Select best locale"""
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
-        print(locale)
         return locale
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def index():
-    """_summary_
-    """
+    """Render index page"""
     return render_template('4-index.html')
 
 
